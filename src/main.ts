28503449c1b11506ca7b8ae53c662b93242c2f2a
Dr0med4r <trompeter-stoll@gmx.de>
2024-06-30 18:15:12 +0200

    add autoreload for css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import Bar from "./Bar"
 import { VolumeControl } from "./bar/Volume"
+import NetworkConfig from "./bar/windows/NetworkConfig"
 
+Utils.monitorFile(`${App.configDir}`, () => {
+    const css = `${App.configDir}/style.css`
+    App.resetCss()
+    App.applyCss(css)
+})
 
 App.config({
     style: "./style.css",
@@ -9,5 +15,6 @@ App.config({
         Bar(0),
         VolumeControl(1),
         VolumeControl(0),
+        NetworkConfig(0),
     ],
 })
