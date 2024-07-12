e7bf9283feffa39248a39d4ed67c4b50a7cc530b
Dr0med4r <trompeter-stoll@gmx.de>
2024-07-13 00:13:13 +0200

    fix: refresh on changed monitors
    Now when there is a monitor change the Bar gets deleted and readded.

--- src/Bar.ts
+++ src/Bar.ts
@@ -5,7 +5,26 @@ import NetworkIndicator from "./bar/Network"
 import System from "./bar/System"
 import Systemtray from "./bar/Systemtray"
 
+const hyprland = await Service.import("hyprland")
 
+function monitors_changed(_, name: any) {
+    // do not execute on startup
+    if (!name) {
+        return
+    }
+
+    const current_monitors = hyprland.monitors.map((v) => String(v.id))
+    const tmp = Widget.Window({
+        name: "tmp",
+        css: "padding: 1px; margin: 1px;",
+    })
+    App.addWindow(tmp)
+    current_monitors.forEach((id) => {
+        App.removeWindow(`bar${id}`)
+        App.addWindow(Bar(Number(id)))
+    })
+    App.removeWindow(tmp)
+}
 
 const Left = () => Widget.Box({
     hpack: "start",
@@ -35,7 +54,7 @@ const Right = () => Widget.Box({
     ]
 })
 
-export default (monitor: number) => Widget.Window({
+const Bar = (monitor: number) => Widget.Window({
     monitor: monitor,
     name: `bar${monitor}`,
     anchor: ['top', 'left', 'right'],
@@ -46,3 +65,8 @@ export default (monitor: number) => Widget.Window({
         end_widget: Right(),
     }),
 })
+    .hook(hyprland, monitors_changed, "monitor-added")
+    .hook(hyprland, monitors_changed, "monitor-removed")
+
+
+export default Bar
